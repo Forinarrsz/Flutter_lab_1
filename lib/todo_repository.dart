7d5b0718555a96ea860e55e089c3c8c12d9f13c4
TishkaaPlay <tix.tot@yandex.ru>
2026-04-08 14:36:44 +0300

    third commit

---
+++ lib/todo_repository.dart
@@ -0,0 +1,37 @@
+import 'todo.dart';
+
+class TodoRepository {
+  final List<Todo> _todos = [];
+
+  List<Todo> getAll() {
+    return _todos;
+  }
+
+  void add(String title) {
+    if (title.trim().isEmpty) {
+      throw ArgumentError('task is empty');
+
+    }
+    // Todo todo = Todo(title.trim())
+  }
+  
+  void complete(int id) {
+    for (var todo in _todos) {
+      if (todo.id == id) {
+        todo.isDone = true;
+        return;
+      }
+      throw ArgumentError(' task with $id not found');
+    }
+
+  }
+
+   void delete(int id) {
+    for (int i = 0; i <_todos.length; i++) {
+      if (_todos[i].id == id) {
+        _todos.removeAt(i);
+        return;
+      }
+    }
+   } 
+}
