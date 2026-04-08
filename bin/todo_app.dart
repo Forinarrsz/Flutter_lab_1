f5be20818d7def0c54e05267328bd52c51ea652c
TishkaaPlay <tix.tot@yandex.ru>
2026-04-08 14:22:02 +0300

    second commit

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,3 +1,14 @@
 void main() {
   print('hello, world!');
+  //x = fkqw;
+  final id = 1;
+  const app = "ToDo";
+
+  print('$id');
+
+  List<String> test = ['app', 'program'];
+  Map<String, dynamic> data = {'key': 13};
+
+  Set<int> id2 = {1, 2, 3};
+  
 }
